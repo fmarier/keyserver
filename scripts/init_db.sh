e802cf233baa87bb0dbf20b0436edcc16350d691
Francois Marier <francois@mozilla.com>
2012-07-03 09:48:17 +1200

    Password not needed for MySQL

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "CREATE DATABASE keyserver CHARACTER SET utf8;" | mysql -uroot -p
-echo "GRANT ALL ON keyserver.* TO nodeuser IDENTIFIED BY 'nodeuser';" | mysql -uroot -p
+echo "CREATE DATABASE keyserver CHARACTER SET utf8;" | mysql -uroot
+echo "GRANT ALL ON keyserver.* TO nodeuser IDENTIFIED BY 'nodeuser';" | mysql -uroot
 mysql -unodeuser -pnodeuser keyserver < schema.sql
